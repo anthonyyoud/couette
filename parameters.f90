17c1d4e21a8999a0da45d265f1b42dcb2f621ae0
Anthony Youd <Anthony.Youd@newcastle.ac.uk>
2003-09-26 08:59:19 +0000

    Tidied up parameters.f90.

--- parameters.f90
+++ parameters.f90
@@ -2,29 +2,29 @@ MODULE parameters
 implicit none
 save
 
-double precision, parameter :: pi = 3.14159265358979d0
-double precision, parameter :: alpha = 0d0
-double precision, parameter :: gamma = 4d0 !(2d0 * pi) / alpha
-double precision, parameter :: eta = 0.75d0
-double precision, parameter :: Re1 = 0d0
-double precision, parameter :: Re2 = 0d0
-double precision, parameter :: Re1_mod = 50d0
-double precision, parameter :: Re2_mod = 0d0
-double precision, parameter :: om1 = 3d0
-double precision, parameter :: om2 = 0d0
-double precision, parameter :: dt = 0.0001d0
-double precision, parameter :: seed = 1d-3
-double precision, parameter :: end_time = 100d0
-double precision, parameter :: tau = 1d0
-integer, parameter :: Ntot = end_time / dt
-integer, parameter :: save_rate = 100
-integer, parameter :: save_rate_2 = 1000
-integer, parameter :: nx = 20
-integer, parameter :: nz = 80
-logical, parameter :: diag = .false.
-logical, parameter :: xsect_save = .false.
-logical, parameter :: restart = .false.
+double precision, parameter :: pi 	   = 3.14159265358979d0
+double precision, parameter :: alpha 	   = 0d0
+double precision, parameter :: gamma 	   = 4d0 !(2d0 * pi) / alpha
+double precision, parameter :: eta 	   = 0.75d0
+double precision, parameter :: Re1 	   = 0d0
+double precision, parameter :: Re2 	   = 0d0
+double precision, parameter :: Re1_mod 	   = 20d0
+double precision, parameter :: Re2_mod 	   = 0d0
+double precision, parameter :: om1 	   = 5d0
+double precision, parameter :: om2 	   = 0d0
+double precision, parameter :: dt 	   = 0.0001d0
+double precision, parameter :: seed 	   = 1d-3
+double precision, parameter :: end_time    = 100d0
+double precision, parameter :: tau 	   = 1d0
+integer, 	  parameter :: nx 	   = 20
+integer, 	  parameter :: nz 	   = 80
+integer, 	  parameter :: save_rate   = 100
+integer, 	  parameter :: save_rate_2 = 200
+logical, 	  parameter :: diag 	   = .false.
+logical, 	  parameter :: xsect_save  = .false.
+logical, 	  parameter :: restart 	   = .false.
 
+integer, 	  parameter :: Ntot = end_time / dt
 double precision, parameter :: delx = 1d0 / (nx+0)
 double precision, parameter :: delz = gamma / (nz+0)
 double precision, parameter :: dx2 = delx ** 2
